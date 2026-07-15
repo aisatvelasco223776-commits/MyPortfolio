704c8f160e3fab22d39eb5304cd35599ecbae045
John Kenneth Velasco <exoqwe67@gmail.com>
2026-07-15 15:14:12 +0800

    Added Featured Porjects and demo URL

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
+const proj = document.querySelector(".proj");
 
+proj.addEventListener('click', function(){
+    window.open("https://images.search.yahoo.com/search/images;_ylt=Awr90JIeCFdqTAIAmZBXNyoA;_ylu=Y29sbwNncTEEcG9zAzEEdnRpZAMEc2VjA3BpdnM-?p=my+projects+template+in+portfolio&fr2=piv-web&type=E210US714G0&fr=mcafee&imgurl=https%3A%2F%2Fessential-addons.com%2Fwp-content%2Fuploads%2F2024%2F01%2Fportfolio-tempalte-sites-831-1024x577.png", "_blank");
+});
